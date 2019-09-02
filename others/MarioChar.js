84a49d1860a9119de6449bb9f6e84b462e27fd81
ankita <ankita231197@gmail.com>
2019-09-02 15:31:25 +0530

    Remove unwanted directory

--- others/MarioChar.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const MarioCharSchema=new Schema({
-                                   name:String,
-                                   weight:Number
-                                 });
-const MarioChar=mongoose.model('marioChar',MarioCharSchema);
-module.exports=MarioChar;
